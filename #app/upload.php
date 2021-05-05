442eac0b4ffaec9c0f93b288583a7f48e88df58e
JackDesBwa <jackdesbwa@gmail.com>
2021-05-05 16:52:47 +0200

    Improve not uploaded cases

--- #app/upload.php
+++ #app/upload.php
@@ -1,26 +1,28 @@
 <?php
 
 auth_requiered();
-
-$d = $_POST['drop'] ?? false;
-if (!check_drop_name($d)) {
-  header($_SERVER['SERVER_PROTOCOL'].' 404 Not Found');
+function not_uploaded() {
+  header($_SERVER['SERVER_PROTOCOL'].' 403 Forbidden');
   exit();
 }
 
+$d = $_POST['drop'] ?? false;
+if (!check_drop_name($d)) not_uploaded();
+
 $target_dir = "#uploads/".$d."/";
-if ($_FILES["file"] ?? false && $_FILES["file"]["error"] == 0) {
+if (($_FILES["file"] ?? false) && $_FILES["file"]["error"] == 0) {
   $target_file = $target_dir . basename($_FILES["file"]["name"]);
   $check = getimagesize($_FILES["file"]["tmp_name"]);
   if($check !== false) {
     if (!is_dir($target_dir)) create_drop($d);
     move_uploaded_file( $_FILES['file']['tmp_name'], $target_file);
   } else {
-    header($_SERVER['SERVER_PROTOCOL'].' 404 Not Found');
-    exit();
+    not_uploaded();
   }
 } else if ($_POST["json"]) {
   file_put_contents($target_dir.'/list.json', $_POST["json"]);
+} else {
+  not_uploaded();
 }
 
 ?>
